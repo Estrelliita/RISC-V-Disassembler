734926f038bf3e754a8a68a0d72616ba87495f93
Estrelliita <estrellalr216@gmail.com>
2024-10-19 23:16:01 -0600

    extract i-type immediate value

--- disassemble.c
+++ disassemble.c
@@ -29,14 +29,18 @@ int disassemble(unsigned int minstr) {
     unsigned int rs1 = (minstr & RS1MASK) >> 15;
     unsigned int rs2 = (minstr & RS2MASK) >> 20;
     unsigned int funct7 = (minstr & FUNCT7MASK) >> 25;
-
-    printf("Opcode: 0x%02X\n", opcode);
-    printf("rd: %d\n", rd);
-    printf("funct3: %d\n", funct3);
-    printf("rs1: %d\n", rs1);
-    printf("rs2: %d\n", rs2);
-    printf("funct7: %d\n", funct7);
+    signed int immediate;
+    //printf("Opcode: 0x%02X\n", opcode);
+    //printf("rd: %d\n", rd);
+    //printf("funct3: %d\n", funct3);
+    //printf("rs1: %d\n", rs1);
+    //printf("rs2: %d\n", rs2);
+    //printf("funct7: %d\n", funct7);
   
     return(0);
+}
+
+signed int extract_i_imm(unsigned int minstr){
+  signed int imm = (minstr & 0xfff00000) >> 20;
 }
             
